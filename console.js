a6a85ea1c0f1916717a88669c41a691fd594f238
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-04-24 10:06:42 +0530

    Using the console

---
+++ console.js
@@ -0,0 +1,12 @@
+//Using the Console 
+console.table({ a: 1, b: 2 });
+console.error('error');
+console.warn('This is a warning');
+
+console.time('Hello');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Hello');
